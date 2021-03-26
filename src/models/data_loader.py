02aeb7e03d575afb1ae79dcd7f3b0bf819bdde6b
Mahasweta Chakraborti <mahaswec@usc.edu>
2021-03-26 17:26:17 +0530

    debug 1

--- src/models/data_loader.py
+++ src/models/data_loader.py
@@ -11,7 +11,7 @@ from others.logging import logger
 class Batch(object):
     def _pad(self, data, pad_id, width=-1):
         if (width == -1):
-            print(d)
+            print(data)
             width = max(len(d) for d in data)
         rtn_data = [d + [pad_id] * (width - len(d)) for d in data]
         return rtn_data
